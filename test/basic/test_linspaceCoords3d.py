96048e8a3d9100e40a4e39a51f4ce6b03ddb85fc
Peter Talley <peterctalley@gmail.com>
2023-11-29 15:24:59 -0500

    Use numpy testing functions

--- test/basic/test_linspaceCoords3d.py
+++ test/basic/test_linspaceCoords3d.py
@@ -1,11 +1,7 @@
 import numpy as np
+from numpy.testing import assert_allclose
 
 from openseestools.basic import linspaceCoords3d
-
-tol = {
-    'rtol': 1e-8,
-    'atol': 1e-10,
-}
 
 
 def test_single_axis():
@@ -13,14 +9,14 @@ def test_single_axis():
     x, _, _ = linspaceCoords3d(0, 0, 0, 4, 0, 0, 4)
     _, y, _ = linspaceCoords3d(0, 0, 0, 0, 4, 0, 4)
     _, _, z = linspaceCoords3d(0, 0, 0, 0, 0, 4, 4)
-    assert np.allclose(x, expected, **tol)
-    assert np.allclose(y, expected, **tol)
-    assert np.allclose(z, expected, **tol)
+    assert_allclose(x, expected)
+    assert_allclose(y, expected)
+    assert_allclose(z, expected)
 
 
 def test_equal_axis():
     expected = np.array([0, 0.5, 1.0, 1.5, 2.0])
     x, y, z = linspaceCoords3d(0, 0, 0, 2, 2, 2, 4)
-    assert np.allclose(x, expected, **tol)
-    assert np.allclose(y, expected, **tol)
-    assert np.allclose(z, expected, **tol)
+    assert_allclose(x, expected)
+    assert_allclose(y, expected)
+    assert_allclose(z, expected)
